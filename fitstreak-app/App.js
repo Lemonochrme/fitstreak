6d9cdb4059a2d08dbf8d2d1ea407d9603c64fc11
Lemonochrome <80280962+Lemonochrme@users.noreply.github.com>
2023-10-23 15:37:09 +0000

    Boilerplate

--- fitstreak-app/App.js
+++ fitstreak-app/App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <View style={styles.MainContainer}>
+      <View style={styles.HeaderContainer}>
+        <View style={styles.GreetingContainer}>
+          <Text style={styles.GreetingText}>Bonjour Robin !</Text>
+        </View>
+        <View style={styles.StreakContainer}>
+
+        </View>
+      </View>
+      <View style={styles.BigText}>
+        <Text style={styles.BigText}>Rien à voir ici. Démarre un entrainement pour commencer.</Text>
+      </View>
+      <View style={styles.NavContainer}>
+        <Button title="Démarrer" />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  MainContainer: {
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    paddingTop: '20%',
+    paddingBottom: '8%',
+    backgroundColor: '#1C1C1C',
+    height: '100%',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  TitleText: {
+    color: '#ffff',
+  },
+  BigText: {
+    color: '#ffff',
+  },
+
+  HeaderContainer: {
+    width: '100%',
+    height: '100%',
+  },
+  GreetingContainer: {
+    height: '100px',
+  },
+  GreetingText: {
+    color: '#ffff',
+  },
+
+  StreakContainer: {
+    backgroundColor: '#393939',
+    height: '24%',
+    width: '100%',
+  },
+
+  ContentContainer: {
+    backgroundColor: '#F9AF5E',
+  },
+  NavContainer: {
+
   },
 });
+
+export default App;
