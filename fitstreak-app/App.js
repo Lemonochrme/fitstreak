5b00c823a039248f2ecf882c7ceb2fe393c0415f
Lemonochrome <80280962+Lemonochrme@users.noreply.github.com>
2023-10-24 15:17:15 +0000

    Layout done !

--- fitstreak-app/App.js
+++ fitstreak-app/App.js
@@ -1,71 +1,116 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-paper';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Button, Text, TouchableOpacity } from 'react-native';
+
+const StreakCard = ({ isStreak }) => {
+  if (isStreak) {
+    return <View style={styles.CardStreak}></View>;
+  } else {
+    return <View style={styles.CardNoStreak}></View>;
+  }
+};
+
+const MainButton = ({ onPress, onLongPress }) => {
+  return (
+    <TouchableOpacity onPress={onPress} onLongPress={onLongPress} style={styles.MainButton}>
+      <Text style={TextStyles.h2}>Démarrer</Text>
+    </TouchableOpacity>
+  );
+};
+
+const BasicTextContent = () => {
+  return (
+    <Text style={TextStyles.h2} color='red'>
+      Rien à voir ici. Démarre un entrainement pour commencer.
+    </Text>
+  );
+}
 
 const App = () => {
+  const [isStreak, setIsStreak] = useState(false);
+
+  const handleStartWorkout = () => {
+    setIsStreak(true);
+  }
+
   return (
-
     <View style={styles.MainContainer}>
-      <View style={styles.HeaderContainer}>
-        <View style={styles.GreetingContainer}>
-          <Text style={styles.GreetingText}>Bonjour Robin !</Text>
-        </View>
-        <View style={styles.StreakContainer}>
+      
+      <View style={styles.Header}>
+        <Text style={TextStyles.h1}>Bienvenue !</Text>
+        <StreakCard isStreak={isStreak} />
+      </View>
 
-        </View>
+      <View style={styles.ContentContainer}>
+        <BasicTextContent />
       </View>
-      <View style={styles.BigText}>
-        <Text style={styles.BigText}>Rien à voir ici. Démarre un entrainement pour commencer.</Text>
+
+      <View style={styles.Footer}>
+        <MainButton onPress={handleStartWorkout} />
       </View>
-      <View style={styles.NavContainer}>
-        <Button title="Démarrer" />
-      </View>
+
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  // Containers
   MainContainer: {
-    paddingLeft: '6%',
-    paddingRight: '6%',
-    paddingTop: '20%',
-    paddingBottom: '8%',
-    backgroundColor: '#1C1C1C',
-    height: '100%',
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#1C1C1C',
+    alignItems: 'center',
+    paddingHorizontal: 20, // Marge sur les côtés
+    justifyContent: 'flex-start',
+  },
+  Header: {
+    marginTop: 50,
+    width: '100%',
+    flex: 2,
+  },
+  ContentContainer: {
+    flex: 4,
+    width: '100%',
+    alignItems: 'center',
+  },
+  Footer: {
+    flex: 1,
+    width: '100%',
     alignItems: 'center',
   },
 
-  TitleText: {
-    color: '#ffff',
-  },
-  BigText: {
-    color: '#ffff',
-  },
-
-  HeaderContainer: {
+  // Cards
+  CardNoStreak: {
+    flex: 1,
     width: '100%',
-    height: '100%',
-  },
-  GreetingContainer: {
-    height: '100px',
-  },
-  GreetingText: {
-    color: '#ffff',
-  },
-
-  StreakContainer: {
     backgroundColor: '#393939',
-    height: '24%',
+    borderRadius: 20,
+  },
+  CardStreak: {
+    flex: 1,
     width: '100%',
+    backgroundColor: '393939',
+    borderRadius: 20,
   },
-
-  ContentContainer: {
-    backgroundColor: '#F9AF5E',
+  MainButton: {
+    backgroundColor: 'orange',
+    paddingHorizontal: 100,
+    paddingVertical: 10, 
+    borderRadius: 10, 
   },
-  NavContainer: {
+});
 
+const TextStyles = StyleSheet.create({
+  h1: {
+    color: '#ffff',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  h2: {
+    color: '#ffff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  h3: {
+    color: '#ffff',
   },
 });
 
